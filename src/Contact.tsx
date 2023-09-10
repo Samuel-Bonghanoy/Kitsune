e53dd464ed53f04c93e7e936fa33d8b91c8e61ae
sethonne <20103261@usc.edu.ph>
2023-09-10 08:25:36 +0800

    feat: add Contact component

---
+++ src/Contact.tsx
@@ -0,0 +1,193 @@
+import { IonIcon } from "@ionic/react";
+import { chevronDownOutline, earthOutline } from "ionicons/icons";
+
+import user1 from "./assets/images/user1.jpg";
+
+import "./PostInput.css";
+
+function PostInput({ onPost }) {
+  return (
+    <div className="posting">
+      <div className="posting-top">
+        <img className="user" src={user1} width="50" height="50" />
+
+        <div className="posting-right">
+          <div className="publicity">
+            <p className="btn-text">Everyone</p>
+            <IonIcon icon={chevronDownOutline} />
+          </div>
+
+          <textarea
+            className="form post"
+            name="post"
+            placeholder="What do you want to write about?"
+          ></textarea>
+
+          <div className="post-bottom">
+            <div className="post-options">
+              <IonIcon icon={earthOutline} />
+              <p className="btn-text">Anyone can reply</p>
+              <IonIcon icon={chevronDownOutline} />
+            </div>
+
+            <button className="post-btn" onClick={onPost}>
+              Post
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="posting">
+        <div className="posting-top">
+          <img className="user" src={user1} width="50" height="50" />
+
+          <div className="posting-right">
+            <div className="publicity">
+              <p className="btn-text">Everyone</p>
+              <IonIcon icon={chevronDownOutline} />
+            </div>
+
+            <textarea
+              className="form post"
+              name="post"
+              placeholder="What do you want to write about?"
+            ></textarea>
+
+            <div className="post-bottom">
+              <div className="post-options">
+                <IonIcon icon={earthOutline} />
+                <p className="btn-text">Anyone can reply</p>
+                <IonIcon icon={chevronDownOutline} />
+              </div>
+
+              <button className="post-btn" onClick={onPost}>
+                Post
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="posting">
+        <div className="posting-top">
+          <img className="user" src={user1} width="50" height="50" />
+
+          <div className="posting-right">
+            <div className="publicity">
+              <p className="btn-text">Everyone</p>
+              <IonIcon icon={chevronDownOutline} />
+            </div>
+
+            <textarea
+              className="form post"
+              name="post"
+              placeholder="What do you want to write about?"
+            ></textarea>
+
+            <div className="post-bottom">
+              <div className="post-options">
+                <IonIcon icon={earthOutline} />
+                <p className="btn-text">Anyone can reply</p>
+                <IonIcon icon={chevronDownOutline} />
+              </div>
+
+              <button className="post-btn" onClick={onPost}>
+                Post
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="posting">
+        <div className="posting-top">
+          <img className="user" src={user1} width="50" height="50" />
+
+          <div className="posting-right">
+            <div className="publicity">
+              <p className="btn-text">Everyone</p>
+              <IonIcon icon={chevronDownOutline} />
+            </div>
+
+            <textarea
+              className="form post"
+              name="post"
+              placeholder="What do you want to write about?"
+            ></textarea>
+
+            <div className="post-bottom">
+              <div className="post-options">
+                <IonIcon icon={earthOutline} />
+                <p className="btn-text">Anyone can reply</p>
+                <IonIcon icon={chevronDownOutline} />
+              </div>
+
+              <button className="post-btn" onClick={onPost}>
+                Post
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="posting">
+        <div className="posting-top">
+          <img className="user" src={user1} width="50" height="50" />
+
+          <div className="posting-right">
+            <div className="publicity">
+              <p className="btn-text">Everyone</p>
+              <IonIcon icon={chevronDownOutline} />
+            </div>
+
+            <textarea
+              className="form post"
+              name="post"
+              placeholder="What do you want to write about?"
+            ></textarea>
+
+            <div className="post-bottom">
+              <div className="post-options">
+                <IonIcon icon={earthOutline} />
+                <p className="btn-text">Anyone can reply</p>
+                <IonIcon icon={chevronDownOutline} />
+              </div>
+
+              <button className="post-btn" onClick={onPost}>
+                Post
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="posting">
+        <div className="posting-top">
+          <img className="user" src={user1} width="50" height="50" />
+
+          <div className="posting-right">
+            <div className="publicity">
+              <p className="btn-text">Everyone</p>
+              <IonIcon icon={chevronDownOutline} />
+            </div>
+
+            <textarea
+              className="form post"
+              name="post"
+              placeholder="What do you want to write about?"
+            ></textarea>
+
+            <div className="post-bottom">
+              <div className="post-options">
+                <IonIcon icon={earthOutline} />
+                <p className="btn-text">Anyone can reply</p>
+                <IonIcon icon={chevronDownOutline} />
+              </div>
+
+              <button className="post-btn" onClick={onPost}>
+                Post
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PostInput;
